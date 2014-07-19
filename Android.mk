5bd2fdce0803235a0d3e8012a7af16ddb5203ee9
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-07-19 16:46:18 +0200

    remove dependency to ncurses, use gnu termcap 1.3.1
    http://ftp.gnu.org/gnu/termcap/
    
    Signed-off-by: Tanguy Pruvot <tanguy.pruvot@gmail.com>

--- Android.mk
+++ Android.mk
@@ -4,14 +4,12 @@ include $(CLEAR_VARS)
 
 lvlibdir := /system/usr/share
 
-LOCAL_CFLAGS = \
+LOCAL_CFLAGS := \
     -Wno-pointer-sign \
     -DLV_HELP_PATH=\"$(lvlibdir)\"  -DUNIX=1 -DTERMCAP=1 -DHAVE_LIBTERMCAP=1 -DSTDC_HEADERS=1 \
     -DHAVE_SYS_WAIT_H=1 -DHAVE_FCNTL_H=1 -DHAVE_SYS_IOCTL_H=1 -DHAVE_SYS_TIME_H=1 -DHAVE_TERMIO_H=1 \
     -DHAVE_UNISTD_H=1 -DHAVE_TERMIOS_H=1 -DHAVE_LOCALE_H=1 -DHAVE_TGETNUM=1 -DHAVE_SETLOCALE=1 \
     -DGETPGRP_VOID=1 -DRETSIGTYPE=void -DHAVE_LANGINFO_CODESET=1
-
-LOCAL_LDLIBS = -ltermcap
 
 LOCAL_SRC_FILES := src/itable.c src/ctable.c src/uty.c src/istr.c src/stream.c src/file.c src/guess.c \
     src/decode.c src/encode.c src/escape.c \
@@ -24,12 +22,11 @@ LOCAL_SRC_FILES := src/itable.c src/ctable.c src/uty.c src/istr.c src/stream.c s
 # From android_support (ndk) nl_langinfo()
 LOCAL_SRC_FILES += build/langinfo.c
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/src \
-    external/libncurses/include \
-    ndk/sources/android/support/include
+# tputs tget* from termcap
+LOCAL_SRC_FILES += termcap/termcap.c termcap/tparam.c
 
-# tputs, tgetstr ...
-LOCAL_SHARED_LIBRARIES += libncurses
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/src $(LOCAL_PATH)/termcap \
+    ndk/sources/android/support/include
 
 LOCAL_MODULE := lv
 LOCAL_MODULE_TAGS := optional
